c3caa45ab0a22b3dcc5962fcbbfdda6b565f42c9
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-12-08 22:02:44 -0500

    used styled function to extend and adapt

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -17,13 +12,15 @@ const Text = styled.span`
   color: white;
 `;
 
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
     </Father>
   );
 }
